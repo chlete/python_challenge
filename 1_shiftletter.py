a48bcea0b89de477057ca4664b02377cf78f22ac
Christian Lete <christian@zvedzo.ch>
2014-10-05 17:04:46 +0200

    final version

--- 1_shiftletter.py
+++ 1_shiftletter.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 
-def shift2toright (line,shift):
+def shift2right (line,shift):
    """
    Shifts the letter a given number
    """
    outline = ""
    for char in line:
-       decimal = (ord(char) + shift) %26  # convert letter into decimal and shift
+       decimal = (ord(char) + shift) % 122  # convert letter into decimal and shift
+       if decimal < 97:			   #if ascii < 97 (uppercase and other prints)
+          decimal += 96			   #shift them
        char = chr(decimal)         # convert the decimal back into char
        outline += str(char)        # append the result to a string
    return(outline) 
